7b8f96fa27ce5a340cff87e71423da70a84ef467
Freddy Huillca <fhuillcan@gmail.com>
2023-06-30 16:44:48 -0500

    Se agrega '21_errors.py

---
+++ 21_errors.py
@@ -0,0 +1,13 @@
+#print(0/0)
+#print(result)
+print('hola')
+
+#suma = lambda x, y: x + y*2
+#assert suma(2, 2) == 4
+
+print('hola 2')
+
+age = 10
+
+if age < 18:
+  raise Exception('No se permite menores de edad')
