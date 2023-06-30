f4d06f3c420c86e2e1c6a9b7d0c4c0675bde7b95
Freddy Huillca <fhuillcan@gmail.com>
2023-06-30 11:49:10 -0500

    Se agrega 09_return.py

---
+++ 09_return.py
@@ -0,0 +1,16 @@
+# Valores por defecto en los argumentos
+def find_volume(length, width, depth):
+  return length * width * depth
+
+
+result = find_volume(10, 20, 30)
+
+print(result)
+
+def find_volume(length = 1, width = 1, depth = 1):
+  return length * width * depth, width, 'hola'
+
+
+result = find_volume(width = 10)
+
+print(result[0])
