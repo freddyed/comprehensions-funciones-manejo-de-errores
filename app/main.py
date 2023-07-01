a38b7fd214de0e917dc7c9cbfed4602f245ea389
Freddy Huillca <fhuillcan@gmail.com>
2023-07-01 11:32:27 -0500

    Se genera una v3.0 del archivo 'main.py' graficando tortas

--- app/main.py
+++ app/main.py
@@ -2,19 +2,23 @@ import utils
 import read_csv
 import charts
 
-
 def run():
+  data = read_csv.read_csv('./app/data.csv')
+  data = list(filter(lambda item : item['Continent'] == 'South America',data))
 
-    data = read_csv.read_csv('./app/data.csv')
-    country = input('Type your country => ')
+  countries = list(map(lambda x: x['Country'], data))
+  percentages = list(map(lambda x: x['World Population Percentage'], data))
+  charts.generate_pie_chart(countries, percentages)
+  '''
+  country = input('Type Country => ')
 
-    result = utils.population_by_country(data, country)
+  result = utils.population_by_country(data, country)
 
-    if len(result) > 0:
-        country = result[0]
-        labels, values = utils.get_population(country)
-        charts.generate_bar_chart(labels, values)
-
+  if len(result) > 0:
+    country = result[0]
+    labels, values = utils.get_population(country)
+    charts.generate_bar_chart(labels, values)
+  '''
 
 
 # Llama almetodo run() si es corrido desde la terminal
