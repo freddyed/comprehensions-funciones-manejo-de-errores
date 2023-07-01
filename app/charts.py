a55463b063dcf01171df5eafe477642badef35ca
Freddy Huillca <fhuillcan@gmail.com>
2023-07-01 00:16:21 -0500

    Se agrega v2.0 de 'charts.py'

--- app/charts.py
+++ app/charts.py
@@ -1,14 +1,18 @@
-import matplotlib
 import matplotlib.pyplot as plt
-matplotlib.use('TkAgg')  # o 'Qt5Agg' si prefieres usar Qt5
 
-def generate_bar_chart():
-    labels = ['a', 'b', 'c']
-    values = [100, 200, 300]
+def generate_bar_chart(labels, values):
+  fig, ax = plt.subplots()
+  ax.bar(labels, values)
+  plt.show()
 
-    fig, ax = plt.subplots()
-    ax.bar(labels, values)
-    plt.show()
+def generate_pie_chart(labels, values):
+  fig, ax = plt.subplots()
+  ax.pie(values, labels=labels)
+  ax.axis('equal')
+  plt.show()
 
 if __name__ == '__main__':
-    generate_bar_chart()
+  labels = ['a', 'b', 'c']
+  values = [10, 40, 800]
+  generate_bar_chart(labels, values)
+  generate_pie_chart(labels, values)
