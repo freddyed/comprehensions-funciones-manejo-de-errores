9bc5924c4d42a6b520fa9f7693834e9d9a79ec23
Freddy Huillca <fhuillcan@gmail.com>
2023-06-30 23:11:20 -0500

    Se agrega '24_write.py' y 'texs.txt.' con modificación de'text.txt

---
+++ 24_write.py
@@ -0,0 +1,15 @@
+# Escribe nuevas lineas debajo
+""" with open('./text.txt', 'r+') as file:
+  for line in file:
+    print(line)
+  file.write('nuevas cosas en este archivo\n')
+  file.write('otra linea\n')
+  file.write(' mas linea\n')
+ """
+# Escribe las lineas sobre el archivo 
+with open('./text.txt', 'w+') as file:
+  for line in file:
+    print(line)
+  file.write('nuevas cosas en este archivo\n')
+  file.write('otra linea\n')
+  file.write(' mas linea\n')
